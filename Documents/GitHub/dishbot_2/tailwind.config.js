9bbb0604e5c377eb45af44e3c3ba9b7814fdf186
alexlucas03 <alexlucas03@gmail.com>
2025-05-22 16:18:01 -0700

    tailwind config added

---
+++ Documents/GitHub/dishbot_2/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  safelist: [
+    'bg-green-500',
+    'bg-yellow-500',
+    'bg-red-500',
+    'bg-gray-300'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
